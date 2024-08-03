be27f658790c10bf096d9c1faccf88443b63cbf6
Siddharth <sahusidd715@gmail.com>
2024-08-03 08:56:20 +0530

    modified:   index.html 	modified:   index.js 	modified:   output.css 	modified:   tailwind.config.js

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-let randoo_no=Math.random()*3;
-if(randoo_no>=0 && randoo_no <=1){
-    console.log(`computer choose bat`)
-}
-else if(randoo_no>1 && randoo_no <=2){
-    console.log("Computer Choose ball")
-}
-else{
-    console.log("computer choose stump")
-}
-let bat_btn=document.querySelector("#bat")
-let ball_brn=document.querySelector("#ball")
-let stump_btn=document.querySelector("#stump")
-bat_btn.addEventListener("click",(randoo_no)=>{
-    if(randoo_no=="")
-})
+// let randoo_no=Math.random()*3;
+// if(randoo_no>=0 && randoo_no <=1){
+//     console.log(`computer choose bat`)
+// }
+// else if(randoo_no>1 && randoo_no <=2){
+//     console.log("Computer Choose ball")
+// }
+// else{
+//     console.log("computer choose stump")
+// }
+// let bat_btn=document.querySelector("#bat")
+// let ball_brn=document.querySelector("#ball")
+// let stump_btn=document.querySelector("#stump")
+// bat_btn.addEventListener("click",(randoo_no)=>{
+//     if(randoo_no=="")
+// })
