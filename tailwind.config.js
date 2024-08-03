65bd830856005ee55089e2e4aba4bcfaa3f58b05
Siddharth <sahusidd715@gmail.com>
2024-08-03 10:26:34 +0530

    new file:   assets/burger-menu.png 	new file:   assets/cat.png 	new file:   assets/cocktail (1).png 	new file:   assets/menu-bar (1).png 	modified:   index.html 	modified:   output.css 	modified:   tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,13 @@ module.exports = {
     extend: {
       colors:{
         purple:""
+      },
+      screens: {
+        '2xl': {'min': '1535px'},
+        'xl': {'min': '1279px'},
+        'lg': {'min': '1023px'},
+        'md': {'min': '767px'},
+        'sm': {'min': '639px'},
       }
     },
   },
